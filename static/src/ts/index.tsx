1149b8d225066e06d1a8a9556166c8cd523d3b31
Chineme1 <64276840+Chineme1@users.noreply.github.com>
2022-08-20 00:58:35 -0700

    Add files via upload

--- static/src/ts/index.tsx
+++ static/src/ts/index.tsx
@@ -11,11 +11,107 @@ import {
   light_text,
   light_title,
 } from "../styles/theme/Colors";
-import React from "react";
+//import React from "react";
 import ReactDOM from "react-dom";
+import React, { useState, useLayoutEffect } from 'react'; //added more
+
+const ThemeContext = React.createContext({
+    dark: false,
+    toggle: () => {},
+});
+
+//export default ThemeContext;
+
+export function ThemeProvider({ children}) {
+  // keeps state of the current theme
+  const [dark, setDark] = useState(false);
+  const [userPicked, setUserPicked] = useState(false);
+
+  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)')
+      .matches;
+  const prefersLight = window.matchMedia('(prefers-color-scheme: light)')
+      .matches;
+  const prefersNotSet = window.matchMedia(
+      '(prefers-color-scheme: no-preference)'
+  ).matches;
+
+  // paints the app before it renders elements
+  useLayoutEffect(() => {
+    if (!userPicked) { // This will stop the system preferences from taking place if the user manually toggles the them
+      if (prefersDark) {
+        setDark(true);
+      }
+  
+      if (prefersLight) {
+        setDark(false);
+      }
+  
+      if (prefersNotSet) {
+        setDark(true);
+      }
+    }
+  
+    applyTheme();
+  }, [dark]);
+
+  // rewrites set of css variablels/colors
+  const applyTheme = () => {
+      let theme;
+      if (dark) {
+          theme = darkTheme;
+      }
+      if (!dark) {
+          theme = lightTheme;
+      }
+
+      const root = document.getElementsByTagName('html')[0];
+      root.style.cssText = theme.join(';');
+  };
+
+  const toggle = () => {
+    console.log('Toggle Method Called');
+
+    const body = document.getElementsByTagName('body')[0];
+    body.style.cssText = 'transition: background .5s ease';
+  
+    setUserPicked(true) // Add this line
+    setDark(!dark);
+  };
+
+  return (
+      <ThemeContext.Provider
+          value={{
+              dark,
+              toggle,
+          }}>
+          {children}
+      </ThemeContext.Provider>
+  );
+}
+
+
+
+const lightTheme = [
+  '--bg-color: var(--color-white)',
+  '--text-color-primary: var(--color-black)',
+  '--text-color-secondary: var(--color-prussianBlue)',
+  '--text-color-tertiary:var(--color-azureRadiance)',
+  '--fill-switch: var(--color-prussianBlue)',
+  '--fill-primary:var(--color-prussianBlue)',
+];
+
+const darkTheme = [
+  '--bg-color: var(--color-mirage)',
+  '--text-color-primary: var(--color-white)',
+  '--text-color-secondary: var(--color-iron)',
+  '--text-color-tertiary: var(--color-white)',
+  '--fill-switch: var(--color-gold)',
+  '--fill-primary:var(--color-white)',
+];
+
 
 const Home: any = () => {
-  const darkModeActive = false;
+  const darkModeActive = true;
 
   const theme = darkModeActive ? "dark" : "light";
 
@@ -102,16 +198,19 @@ const Home: any = () => {
       <Main className="main" theme={theme}>
         <div className="content">
           <Title className="title" theme={theme}>
-            Welcome! This is a title
+            Welcome! This is the header
           </Title>
 
           <Subtitle className="subtitle" theme={theme}>
-            This is a subtitle
+            This is a subtitle for Slim Shady.
+            Dark feels readddy.
+            About to spit a verse on your baby.
+            The real Shigga Shigga Shiggga Slim Shady.
           </Subtitle>
 
           <H1 theme={theme}>This is H1</H1>
           <Text className="body" theme={theme}>
-            This is regular text
+            This is regular text.
           </Text>
         </div>
       </Main>
